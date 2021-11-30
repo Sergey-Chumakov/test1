d6c93f92375a0d87425c1239e3e3e27f58325b2a
schumakov <01041981si@gmail.com>
2021-12-01 01:36:43 +0700

    Second commit

--- src/Cat.java
+++ src/Cat.java
@@ -1,7 +1,11 @@
 public class Cat {
     public static void main(String[] args) {
-        int = a;
+        int a = 10;
         a = 10;
+        System.out.println(a);
+        1111111111111111111111
+
+
     }
 }
 
